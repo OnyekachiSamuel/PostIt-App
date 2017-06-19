ecb7ba3325bf599756c9f0961760855488dc0696
Ezeoke Onyekachi Samuel <ezeokesam@gmail.com>
2017-06-19 08:42:48 +0100

    add model for users to add users to group

---
+++ server/models/groupMembers.js
@@ -0,0 +1,51 @@
+import Sequelize from 'sequelize';
+import config from '../config';
+
+const sequelize = new Sequelize(config.url);
+const Groups = sequelize.define('GroupMembers', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    onDelete: 'CASCADE',
+    references: {
+      model: 'Users',
+      key: 'id',
+      as: 'userId'
+    }
+  },
+  groupId: {
+    type: Sequelize.INTEGER,
+    onDelete: 'CASCADE',
+    references: {
+      model: 'Group',
+      key: 'id',
+      as: 'groupId'
+    }
+  }
+}, {
+  classMethods: {
+    associate: (models) => {
+      // associations can be defined here
+      Groups.belongsTo(models.Users, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+      });
+      Groups.belongsTo(models.Group, {
+        foreignKey: 'groupId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+});
+
+export default Groups;
+
