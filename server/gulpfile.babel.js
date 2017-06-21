236721f4e0a1b20f051bccbf19b4ac27e4f50afe
Ezeoke Onyekachi Samuel <ezeokesam@gmail.com>
2017-06-21 08:06:51 +0100

    add task to run test and coverage reporting

--- server/gulpfile.babel.js
+++ server/gulpfile.babel.js
@@ -6,14 +6,14 @@ import coveralls from 'gulp-coveralls';
 import istanbul from 'gulp-babel-istanbul';
 import injectModules from 'gulp-inject-modules';
 import jasmine from 'gulp-jasmine';
+import cover from 'gulp-coverage';
 import exit from 'gulp-exit';
-import mocha from 'gulp-mocha';
-// import babel from 'babel-register';
+
 
 
 
 gulp.task('transpile', () => {
-  gulp.src(['models/*js', 'routes/index.js', 'spec/routesTestSpec.js', 'tests/*js', 'app.js', 'config.js'])
+  gulp.src(['models/*js', 'routes/index.js', 'controllers/controller.js', 'spec/*.js', 'tests/*js', 'app.js'])
     .pipe(babel({
       presets: ['es2015']
     }))
@@ -22,29 +22,10 @@ gulp.task('transpile', () => {
 
 // Run the tests
 gulp.task('run-test', ['transpile'], () => {
-  gulp.src(['dist/routesTestSpec.js'])
+  gulp.src(['dist/modelsTestSpec.js'])
     .pipe(babel())
     .pipe(jasmine())
     .pipe(exit());
-});
-
-
-
-
-
-gulp.task('serve', () =>
-  nodemon({
-    script: 'app.js',
-    ext: 'js html',
-    env: { NODE_ENV: process.env.NODE_ENV }
-  })
-);
-
-gulp.task('test-run', () => {
-  gulp.src(['tests/*.js'])
-    .pipe(mocha({
-      compilers: babel({ presets: ['es2015'] })
-    }));
 });
 
 // Generate the coverage report
@@ -53,22 +34,40 @@ gulp.task('test', () => {
     .pipe(istanbulReport());
 });
 
+gulp.task('serve', ['transpile'], () =>
+  nodemon({
+    script: 'dist/app.js',
+    ext: 'js html',
+    env: { NODE_ENV: process.env.NODE_ENV }
+  })
+);
 
 // Generate the coverage report
 gulp.task('coverage', (cb) => {
-  gulp.src(['models/*js', 'routes/index.js', 'app.js'])
+  gulp.src(['routes/index.js', 'app.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', () => {
-      gulp.src('tests/*.js')
+      gulp.src('spec/routesTestSpec.js')
         .pipe(babel())
         .pipe(injectModules())
-        .pipe(mocha())
+        .pipe(jasmine())
         .pipe(istanbul.writeReports())
         .pipe(istanbul.enforceThresholds({ thresholds: { global: 30 } }))
         .on('end', cb);
     });
 });
+/* gulp.task('jasmine', () => {
+  gulp.src('spec/*.js')
+    .pipe(cover.instrument({
+      pattern: ['models/*js', 'routes/index.js', 'app.js'],
+      debugDirectory: 'debug'
+    }))
+    .pipe(jasmine())
+    .pipe(cover.gather())
+    .pipe(cover.format())
+    .pipe(gulp.dest('reports'));
+});*/
 
 // Load code coverage to coveralls
 gulp.task('coveralls', ['coverage'], () => {
@@ -81,3 +80,5 @@ gulp.task('coveralls', ['coverage'], () => {
     .pipe(coveralls());
 });
 
+
+gulp.task('default', ['transpile', 'run-test', 'coveralls', 'test', 'coverage']);
