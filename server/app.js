24be3636d004d478bdf97acf306134163354e1e5
Ezeoke Onyekachi Samuel <ezeokesam@gmail.com>
2017-06-29 16:30:54 +0100

    clean up code

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import env from 'dotenv';
+import routes from './routes/index';
+
+env.config();
+const app = express();
+const port = process.env.port || 3000;
+
+
+// Middlewares used
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'yyuuuatsttgsbbbabbaNNANMSMKWKKZLALLLLZZNZN',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/', routes);
+app.use('/api/', routes);
+app.listen(port, () => {
+  console.log('Listening on port 3000');
+});
+
+export default app;
+
+
+
+
+
