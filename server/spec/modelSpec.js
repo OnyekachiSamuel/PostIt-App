5ad7be67ab6d7d8ca3f2c79b155d58e1262cc1fa
Ezeoke Onyekachi Samuel <ezeokesam@gmail.com>
2017-06-29 15:22:20 +0100

    add more test specs

--- server/spec/modelSpec.js
+++ server/spec/modelSpec.js
@@ -1,55 +1,66 @@
 import jasmine from 'jasmine';
 import Sequelize from 'sequelize';
 import expect from 'expect';
-import Users from '../models/users';
-import Group from '../models/group';
-// import GroupMembers from '../models/groupMembers';
-// import Messages from '../models/messages';
-import db from '../config/db_url.json';
+import Users from '../models/tests/users';
+import Group from '../models/tests/group';
+import GroupMembers from '../models/groupMembers';
+import Messages from '../models/messages';
+import db from '../config/test_db_url.json';
 
-
+// TESTING all the models used in the application
 describe('Model test suite', () => {
-  beforeEach((done) => {
+  beforeAll((done) => {
     const sequelize = new Sequelize(db.url);
     sequelize.authenticate().then(() => { console.log('Connection established'); })
     .catch((err) => { console.log('Error occured', err); });
     done();
-  }, 10000);
+  });
   it('I should be able to create a new user with this model', (done) => {
     Users.sync({ force: true }).then(() => {
-      Users.create({ name: 'Peter', username: 'Ike', email: 'alobam@gmail.com', password: 'show' })
+      Users.create({ name: 'Helen', username: 'Sam', email: 'aloba@gmail.com', password: 'showa' })
       .then((user) => {
         if (user) {
-          expect('Ike').toBe(user.dataValues.username);
+          expect('Sam').toBe(user.dataValues.username);
+          expect('aloba@gmail.com').toBe(user.dataValues.email);
         }
         done();
-      }).catch((err) => { console.log('Failed', err); done(); });
+      }).catch((err) => { done(err); });
     });
   }, 10000);
   it('I should be able to create a new group with this model', (done) => {
     Group.sync({ force: true }).then(() => {
-      Group.create({ groupName: 'Zikites', groupCategory: 'Class of 2014', userId: 1 })
+      Group.create({ groupName: 'Zikites', groupCategory: 'Class of 2015', userId: 1 })
       .then((group) => {
-        if (group) {
-          expect('Zikites').toNotBe('Zike');
-          expect('Class of 2014').toBe(group.dataValues.groupCategory);
-        }
+        expect('Zikites').toNotBe('Zike');
+        expect('Class of 2015').toBe(group.dataValues.groupCategory);
+        expect(group.dataValues.userId.toString()).toBe('1');
         done();
       });
-    }).catch((err) => { console.log('Failed', err); });
+    }).catch((err) => { done(err); });
   }, 10000);
- /* it('I should be able to add users to group I created', (done) => {
+  it('I should be able to add users to group I created', (done) => {
     GroupMembers.sync({ force: true }).then(() => {
       GroupMembers.create({ userId: 1, admin: 1, groupId: 1 })
       .then((members) => {
-        if (members) {
-          expect(1).toBe(members.dataValues.userId);
-        }
+        expect(members.dataValues.userId.toString()).toBe('1');
+        expect(members.dataValues.admin.toString()).toBe('1');
+        expect(members.dataValues.groupId.toString()).toBe('1');
         done();
       });
     }).catch((err) => {
-      console.log('Failed', err);
+      done(err);
     });
-  }, 10000);*/
+  }, 10000);
+  it('I should be able to post message to groups with this model', (done) => {
+    Messages.sync({ force: true }).then(() => {
+      Messages.create({ message: 'I knew it will happened', userId: 1, groupId: 1 })
+      .then((message) => {
+        expect(message.dataValues.message).toBe('I knew it will happened');
+        expect(message.dataValues.userId.toString()).toBe('1');
+        expect(message.dataValues.groupId.toString()).toBe('1');
+        done();
+      });
+    }).catch((err) => { done(err); });
+  }, 10000);
 });
 
--- server/spec/routeSpec.js
+++ server/spec/routeSpec.js
@@ -1,68 +1,110 @@
 import supertest from 'supertest';
 import jasmine from 'jasmine';
-import { expect } from 'chai';
-// import app from '../app';
+import expect from 'expect';
 import app from '../app';
-
 
 const request = supertest(app);
 
-
 describe('Test routes', () => {
   describe('POST /api/signup', () => {
-    it('Should return a json object with success message', (done) => {
+    it('Should return a signup data', (done) => {
       request.post('/api/signup')
-        .send({ name: 'Eze', username: 'Bona', email: 'jcf@gmail.com', password: 'azundu' })
+        .send({ name: 'Eze', username: 'Kenet', email: 'jyyyu@gmail.com', password: 'azundu' })
         .expect(200)
         .end((err, res) => {
-          expect(res.body.username === 'Bona');
+          expect('success').toBe(res.body.status);
+          expect('Account created').toBe(res.body.message);
+          expect('Jane').toNotBe(res.body.data.name);
+          expect('Kenet').toBe(res.body.data.username);
+          done(err);
+        });
+    }, 10000);
+  });
+  describe('POST /api/signin', () => {
+    it('Should not be able to login with wrong password', (done) => {
+      request.post('/api/signin')
+        .send({ username: 'Kenet', password: 'yayyaya' })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.status).toBe('Invalid Password');
+          done(err);
+        });
+    }, 10000);
+    it('Should not be able to login with wrong credential', (done) => {
+      request.post('/api/signin')
+        .send({ username: 'Keneta', password: 'kachi' })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.status).toBe('Invalid password or Username');
+          done(err);
+        });
+    }, 10000);
+    it('Should be able to login to account created', (done) => {
+      request.post('/api/signin')
+        .send({ username: 'Kenet', password: 'azundu' })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.status).toBe('Success');
+          expect(res.body.data.username).toBe('Kenet');
+          expect(res.body.message).toBe('Logged In');
           done(err);
         });
     }, 10000);
   });
   describe('POST /api/group', () => {
-    it('Should return an object containing the group created details', (done) => {
+    it('Should be able to create group by registered user', (done) => {
       request.post('/api/group')
-        .send({ groupName: 'Andela Tech Group', groupCategory: 'Tech', userId: 1 })
-        .expect(200)
-        .end((err, res) => {
-          expect(res.body.groupName === 'Andela Tech Group');
-          done(err);
-        });
+      .send({ groupName: 'Andela21', groupCategory: 'Full stack with js', userId: 1 })
+      .expect(200)
+      .end((err, res) => {
+        expect('success').toBe(res.body.status);
+        expect('Group Created').toBe(res.body.message);
+        done();
+      });
     }, 10000);
   });
-  describe('POST /api/group/:groupId/user', () => {
-    it('Should return a success message', (done) => {
-      request.post('/api/group/:groupId/user')
-        .send({ admin: 1, userId: 1, groupId: 1 })
-        .end((err, res) => {
-          expect(res.body.message !== null && res.body.status === 'Success');
-          done(err);
-        });
+  describe('POST /api/group/1/user', () => {
+    it('Should be able to add a user to groups', (done) => {
+      request.post('/api/group/1/user')
+      .send({ admin: 1, userId: 1 })
+      .expect(200)
+      .end((err, res) => {
+        expect('success').toBe(res.body.status);
+        expect('User/Users added').toBe(res.body.message);
+        done();
+      });
     }, 10000);
   });
-  describe('POST /api/group/:groupId/messages', () => {
-    it('Should return a success message ', (done) => {
+  describe('POST /group/1/messages', () => {
+    it('Should be able to post message to created group', (done) => {
       request.post('/api/group/1/messages')
-        .send({ groupId: 1, userId: 1, message: 'I am just testing you out' })
-        .expect(200)
-        .end((err, res) => {
-          expect(res.body.status === 'Success' && res.body.message === 'I am just testing you out');
-          done(err);
-        });
+      .expect(200)
+      .send({ message: 'This is ANDELA', priority: 'Normal', userId: 1, groupId: 1 })
+      .end((err, res) => {
+        expect('success').toBe(res.body.status);
+        expect('Message sent').toBe(res.body.message);
+        expect(1).toBe(res.body.data.userId);
+        expect('Normal').toBe(res.body.data.priority);
+        expect('This is ANDELA').toBe(res.body.data.message);
+        done();
+      });
     }, 10000);
   });
-  describe('GET /api/group/:groupId/mesages', () => {
-    it('Should return a success message', (done) => {
+  describe('GET /api/group/:groupId/messages', () => {
+    it('Should be able to get messages in a particular group', (done) => {
       request.get('/api/group/1/messages')
-        .expect(200)
-        .end((err, res) => {
-          expect(res.body.message === 'I am just testing you out');
-          done(err);
-        });
-    }, 10000);
+      .expect(200)
+      .end((err, res) => {
+        expect('This is ANDELA').toBe(res.body.data[0].message);
+        expect('Normal').toBe(res.body.data[0].priority);
+        expect(1).toBe(res.body.data[0].groupId);
+        expect(1).toBe(res.body.data[0].userId);
+        done();
+      });
+    }, 1000);
   });
 });
+
 
 
 
