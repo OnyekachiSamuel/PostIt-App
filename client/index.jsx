64d6a4b14828f979b6db67182b13f3632964a812
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2017-07-11 03:06:30 +0100

    implement routing of components

--- client/index.jsx
+++ client/index.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import App from './components/App.jsx';
 import style from './styles/main.css';
-import HomePage from './components/HomePage.jsx';
-// import CreateGroup from './components/CreateGroup.jsx';
-// import PostMessage from './components/PostMessage.jsx';
-// import ViewMessages from './components/ViewMessages.jsx';
 import js from './js/main';
 
-
-class Home extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Route path="/" component={HomePage}>
-        </Route>
-      </Router>
-    );
-  }
-}
-ReactDOM.render(<Home/>, document.getElementById('app'));
+ReactDOM.render(
+<Router><App/></Router>, document.getElementById('app'));
