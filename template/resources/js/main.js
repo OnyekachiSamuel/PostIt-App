cc3d86f8dddb709e0ff7b5554efe3f182ff26ad3
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2017-07-13 09:25:00 +0100

    modify page templates

--- template/resources/js/main.js
+++ template/resources/js/main.js
@@ -1,5 +1,8 @@
   $(document).ready(() => {
     $('select').material_select();
 	$('.collapsible').collapsible();
-	$('.modal').modal();
+	$('.modal').modal(
+        {
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+        });
   });
