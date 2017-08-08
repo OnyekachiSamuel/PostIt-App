c4ed37a3bf480999582cc70d5f333155fcd063db
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2017-08-08 11:41:08 +0100

    add db url for online hosting of app

--- server/models/message.js
+++ server/models/message.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import config from '../config/db.json';
+import config from '../config/db_url.json';
 
 const sequelize = new Sequelize(config.url);
 const Message = sequelize.define('Message', {
