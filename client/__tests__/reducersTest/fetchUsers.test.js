009c60778245164f0f51cce6238336de74501fa2
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2017-08-14 00:24:30 +0100

    fix failing test

--- client/__tests__/reducersTest/fetchUsers.test.js
+++
@@ -1,3 +0,0 @@
-import expect from 'expect';
-import { fetchUsers } from '../../actions/fetchUsers';
-import fetchUsersReducer from '../../reducers/fetchUsersReducer';
--- client/actions/signupAction.js
+++ client/actions/signupAction.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 import { SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from './actionTypes';
 
 export const signUp = (userData) => {
@@ -18,7 +19,7 @@ export const userSignupRequest = (userData) => {
   return (dispatch) => {
     return axios.post('/api/signup', userData).then((payload) => {
       if (payload.data.status === 'success') {
-        dispatch(signUp(payload.data.token));
+        dispatch(signUp(jwt.decode(payload.data.token)));
         localStorage.setItem('token', payload.data.token);
         localStorage.setItem('username', payload.data.data.username);
         location.href = '/group';
