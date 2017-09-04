75b2d00da2d8203c7af953bc34b57f7c4af39666
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2017-09-04 22:19:41 +0100

    feature(client-test): implement client tests - implement tests for actions and reducers - implement tests for react components

---
+++ client/__tests__/LandingPageTest/SignInModal.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { SignInModal } from '../../components/Index/SignInModal.jsx';
+
+const props = {
+  signInRequest: jest.fn(),
+  googleAuthRequest: jest.fn(),
+  forgetPasswordRequest: jest.fn()
+};
+
+describe('<SignInModal />', () => {
+  it('component should render correctly', () => {
+    const wrapper = shallow(<SignInModal { ...props } />);
+    expect(wrapper.node.props.className).toEqual('row modal');
+    expect(wrapper.node.props.id).toEqual('modal2');
+  });
+  describe('Test for onChange event', () => {
+    it('should update the state on in input', () => {
+      const wrapper = shallow(<SignInModal { ...props }/>);
+      const event = {
+        target: {
+          name: 'name',
+          value: 'Dan',
+        }
+      };
+      wrapper.instance().onChange(event);
+      expect(wrapper.state().name).toEqual('Dan');
+      expect(wrapper.node.props.className).toEqual('row modal');
+      expect(wrapper.node.props.id).toEqual('modal2');
+    });
+  });
+  describe('Test for onSubmit function', () => {
+    it('should be called on button click', () => {
+      const mockOnSubmit = sinon.spy(() => {});
+      const wrapper = shallow(<SignInModal onSubmit = { mockOnSubmit } {...props} />);
+      const btn = wrapper.find('.btn');
+      btn.simulate('click', mockOnSubmit());
+      expect(mockOnSubmit.calledOnce).toBe(true);
+      expect(btn.node.props.className).toBe('btn waves-effect waves-light modal-close');
+      expect(btn.node.props.type).toBe('submit');
+      expect(btn.node.props.name).toBe('action');
+      expect(btn.node.props.children).toBe('Submit');
+    });
+  });
+});
+
